b0d139e6dfe91af7c461b6c05ebca6c1fbfca96e
Gaston Marino <gmarino86@gmail.com>
2022-06-13 16:31:33 -0300

    Ajustes de CSS

--- resources/views/layout/discography.blade.php
+++ resources/views/layout/discography.blade.php
@@ -8,7 +8,7 @@
     <?php
     foreach ($discography as $disco) : ?>
     
-        <div class="card grid-item col col-sm-6 col-md-4 mb-3">
+        <div class="card grid-item mb-2">
             <img src="{{ $disco['cover']->url }}" class="card-img-top" alt="{{ $disco['artist_name'] }}" width="200" height="200"/>
             <div class="card-body">
                 <h2 class="card-title">{{ $disco['artist_name'] }}</h2>
